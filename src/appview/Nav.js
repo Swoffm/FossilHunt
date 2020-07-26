ab4603573214659c7d55c77cb19c37e59bd87f81
Gavin Swofford <gavinswofford@gmail.com>
2020-07-26 18:22:44 -0500

    Home Page is almost finished. Users can now view the Home Page and see information. Information and facts about the website still need to be fillied in

--- src/appview/Nav.js
+++ src/appview/Nav.js
@@ -2,14 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./styles/nav.css"
 
+
+
 const Nav = () => {
     return (
         <header >
             <div className="logo">
-            <Link className="nav-link" to="/">
+            {/* <Link className="nav-link" to="/">
             <img id="logo" src="https://res.cloudinary.com/fossilhunt/image/upload/v1595625573/IMG_4891_mofnwm.jpg" alt="Fossil Hunt Logo"/>
 
-                        </Link>
+                        </Link> */}
              </div>
             <nav>
                 <ul className="container">
