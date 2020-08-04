ae66cf173032b8ea450f48909077e2c817e164ea
Gavin Swofford <gavinswofford@gmail.com>
2020-08-03 21:50:35 -0500

    users can now change their profile picture

--- src/components/accountInformation/pages/accountEditJSX.js
+++ src/components/accountInformation/pages/accountEditJSX.js
@@ -28,6 +28,70 @@ const AccountEditJSX = props => {
 
                     value={props.user.username}
                 />
+                <section className = "cloud--image-selection">
+                <label htmlFor="cloud--image">Select Profile Image</label> 
+                <section className="cloud--imageSection">
+                   
+                    <div>
+                        <img className="cloud--image" src="https://res.cloudinary.com/drcnakesm/image/upload/v1596503386/IMG_1123_yaolse.jpg" alt="profile fossil image" />
+                        <input onChange={props.handleFieldChange}  id="userImage" type="radio" name="cloud--image" value="https://res.cloudinary.com/drcnakesm/image/upload/v1596503386/IMG_1123_yaolse.jpg" className="cloud--image"></input>
+                       
+                        <img className="cloud--image" src="https://res.cloudinary.com/drcnakesm/image/upload/v1596503386/IMG_1116_vgck5i.jpg" alt="profile fossil image" />
+                        <input id="userImage" name="cloud--image" onChange={props.handleFieldChange}  type="radio" value="https://res.cloudinary.com/drcnakesm/image/upload/v1596503386/IMG_1116_vgck5i.jpg" className="cloud--image"></input>
+                       
+                        </div>
+                       
+                        
+                        <div>
+                        <img className="cloud--image" src="https://res.cloudinary.com/drcnakesm/image/upload/v1596503386/IMG_1118_ikvo4j.jpg" alt="profile fossil image" />
+                        <input name="cloud--image" id="userImage" onChange={props.handleFieldChange}  type="radio" value="https://res.cloudinary.com/drcnakesm/image/upload/v1596503386/IMG_1118_ikvo4j.jpg"  className="cloud--image"></input>
+                      
+                        <img className="cloud--image" src="https://res.cloudinary.com/drcnakesm/image/upload/v1596503386/IMG_1120_of7gsg.jpg" alt="profile fossil image" />
+                        <input name="cloud--image" id="userImage" onChange={props.handleFieldChange}  type="radio" value="https://res.cloudinary.com/drcnakesm/image/upload/v1596503386/IMG_1120_of7gsg.jpg" className="cloud--image"></input>
+                        </div>
+
+                        <div>
+                        <img className="cloud--image" src="https://res.cloudinary.com/drcnakesm/image/upload/v1596503386/IMG_1119_djf1x9.jpg" alt="profile fossil image" />
+                        <input name="cloud--image" id="userImage" onChange={props.handleFieldChange}  type="radio" value="https://res.cloudinary.com/drcnakesm/image/upload/v1596503386/IMG_1119_djf1x9.jpg" className="cloud--image"></input>
+                      
+                        <img className="cloud--image" src="https://res.cloudinary.com/drcnakesm/image/upload/v1596503386/IMG_1117_qcwpve.jpg" alt="profile fossil image" />
+                        <input name="cloud--image" id="userImage" onChange={props.handleFieldChange}  type="radio" value="https://res.cloudinary.com/drcnakesm/image/upload/v1596503386/IMG_1117_qcwpve.jpg"  alt="profile fossil image" className="cloud--image"></input>
+                     
+                        </div>
+                    
+
+                        <div>
+                        <img className="cloud--image" src="https://res.cloudinary.com/drcnakesm/image/upload/v1596503386/IMG_1115_f0qpi3.jpg" alt="profile fossil image" />
+                        <input name="cloud--image" id="userImage" onChange={props.handleFieldChange}  type="radio" value="https://res.cloudinary.com/drcnakesm/image/upload/v1596503386/IMG_1115_f0qpi3.jpg" className="cloud--image"></input>
+                      
+                        
+                        <img className="cloud--image" src="https://res.cloudinary.com/drcnakesm/image/upload/v1596503386/IMG_1113_fvrm6r.jpg" alt="profile fossil image" />
+                        <input name="cloud--image" id="userImage" onChange={props.handleFieldChange}  type="radio" value="https://res.cloudinary.com/drcnakesm/image/upload/v1596503386/IMG_1113_fvrm6r.jpg" className="cloud--image"></input>
+                        </div>
+
+
+
+                        <div>
+                        <img className="cloud--image" src="https://res.cloudinary.com/drcnakesm/image/upload/v1596503385/IMG_1121_srfw3p.jpg" alt="profile fossil image" />
+                        <input name="cloud--image" id="userImage" onChange={props.handleFieldChange}  type="radio" value="https://res.cloudinary.com/drcnakesm/image/upload/v1596503385/IMG_1121_srfw3p.jpg" className="cloud--image"></input>
+                       
+                       
+                        <img className="cloud--image" src="https://res.cloudinary.com/drcnakesm/image/upload/v1596503385/IMG_1122_emkupd.jpg" alt="profile fossil image" />
+                        <input name="cloud--image" id="userImage" onChange={props.handleFieldChange}  type="radio" value="https://res.cloudinary.com/drcnakesm/image/upload/v1596503385/IMG_1122_emkupd.jpg"  className="cloud--image"></input>
+                      
+                        </div>
+                        <div>
+                        <img className="cloud--image" src="https://res.cloudinary.com/drcnakesm/image/upload/v1596503385/IMG_1125_r6dvj4.jpg" alt="profile fossil image" />
+                        <input name="cloud--image" id="userImage" onChange={props.handleFieldChange}  type="radio" value="https://res.cloudinary.com/drcnakesm/image/upload/v1596503385/IMG_1125_r6dvj4.jpg" className="cloud--image"></input>
+                        </div>
+                      
+                       
+                       
+
+                </section>
+                </section>
+                
+                
 
 
     <div>Profile Image: </div>
