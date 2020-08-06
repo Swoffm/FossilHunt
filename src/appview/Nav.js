e1d29b629d340f28d1f4ea9a33b0a1ce27b8d433
Gavin Swofford <gavinswofford@gmail.com>
2020-08-05 21:21:05 -0500

    navbar has been styled for both mobile and desktop

--- src/appview/Nav.js
+++ src/appview/Nav.js
@@ -1,48 +1,127 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./styles/nav.css"
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
 
-
-const Nav = (props) => {
+const FossilNav = (props) => {
     // let haasUser = true;
     let user = sessionStorage.getItem("userInfo")
+
+    // below code is used for dropdown menu in mobile
+    function dropDown() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        }
+         else {
+            x.className = "topnav";
+        }
+    }
+
+
     return (
-        <header>
-            <div className="logoParent">
-                <div className="logo">
-                    <Link className="nav-link" to="/">
-                        <img id="logo" src="https://res.cloudinary.com/drcnakesm/image/upload/v1596054493/The_Hunt_t90gdj.jpg" alt="Fossil Hunt Logo" />
+        <div>
 
-                    </Link>
-                </div> <nav>
-                        <ul className="container">
-                            <li>
-                                <Link className="nav-link" to="/">
-                                    Home
+            <div className="topnav" id="myTopnav">
+            <Link className="nav-link" to="/">
+                            <img id="logo" src="https://res.cloudinary.com/drcnakesm/image/upload/v1596054493/The_Hunt_t90gdj.jpg" alt="Fossil Hunt Logo" />
+
                         </Link>
-                            </li>
-                            <li>
-                                <Link className="nav-link" to="/forum">
-                                    Fossil Forum
-                        </Link>
-                            </li>
-                            {
-                                user ? <li><Link className="nav-link" to="/fossilcollection"> Fossil Collection</Link></li> : null
-                            }
+                <Link className="active" to="/">
+                            Home
+                         </Link>
 
-                            {
-                                !user ? <li><Link className="nav-link" to="/login"> Log In </Link></li> :  <li><Link className="nav-link" to="/account"> Account Info </Link></li>
-                            }
-                            
+                        <Link className="nav-link" to="/forum">
+                            Fossil Forum
+                            </Link>
 
-                        </ul>
-                    </nav>
-                
+                        {
+                            user ? <Link className="nav-link" to="/fossilcollection"> Fossil Collection</Link> : null
+                        }
 
+                        {
+                            !user ? <Link className="nav-link" to="/login"> Log In </Link> : <Link className="nav-link" to="/account"> Account Info </Link>
+                        }
+
+                {/* <div className="dropdown">
+                    <button className="dropbtn">Dropdown
+                     <i className="fa fa-caret-down"></i>
+                    </button>
+                    <div className="dropdown-content">
+                        <Link className="active" to="/">
+                            Home
+                         </Link>
+
+                        <Link className="nav-link" to="/forum">
+                            Fossil Forum
+                            </Link>
+
+                        {
+                            user ? <li><Link className="nav-link" to="/fossilcollection"> Fossil Collection</Link></li> : null
+                        }
+
+                        {
+                            !user ? <li><Link className="nav-link" to="/login"> Log In </Link></li> : <li><Link classNameName="nav-link" to="/account"> Account Info </Link></li>
+                        }
+
+
+
+                    </div>
+                </div> */}
+
+                <a className="icon" onClick={dropDown}>&#9776;</a>
             </div>
-        </header>
+
+
+
+            {/* <header>
+                <div className="logoParent">
+                    <div className="logo">
+                        <Link className="nav-link" to="/">
+                            <img id="logo" src="https://res.cloudinary.com/drcnakesm/image/upload/v1596054493/The_Hunt_t90gdj.jpg" alt="Fossil Hunt Logo" />
+
+                        </Link>
+                    </div>
+                </div>
+                <nav className="topnav" id="myTopnav">
+                    <ul className="container--wrapper">
+                        <li className="firstItem">
+
+
+
+                            <ul className="dropDown">
+
+
+                                <li>
+                                    <Link className="nav-link" to="/">
+                                        Home
+                         </Link>
+                                </li>
+                                <li>
+                                    <Link className="nav-link" to="/forum">
+                                        Fossil Forum
+                            </Link>
+                                </li>
+                                {
+                                    user ? <li><Link className="nav-link" to="/fossilcollection"> Fossil Collection</Link></li> : null
+                                }
+
+                                {
+                                    !user ? <li><Link className="nav-link" to="/login"> Log In </Link></li> : <li><Link classNameName="nav-link" to="/account"> Account Info </Link></li>
+                                }
+
+
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+
+
+
+            </header> */}
+        </div>
     );
 };
 
-export default Nav;
+export default FossilNav;
--- src/components/fossilCollection/pages/FossilCollectionJSX.js
+++ src/components/fossilCollection/pages/FossilCollectionJSX.js
@@ -5,36 +5,36 @@ import { Link } from "react-router-dom";
 const FossilCollectionJSX = (props) => {
 
 
-    return (
-      
-        <section className="fossilCard">
-            <h3>{props.fossil.genus}</h3>
-            <div className="fossilImage">
-            {props.fossil.image == null || props.fossil.image == "" ? 
-            null : <img className="fossil--image" src={props.fossil.image} alt="Fossil Hunt Logo" />}
-              
-             
-            </div>
-    <div className="locationInfo">Location: {props.fossil.location}</div>
+  return (
 
-    <section className="btn">
-    <Link to={`/fossilDetails/${props.fossil.id}`}>
-  <button className="detailsButton">Details</button>
-</Link>
+    <section className="fossilCard">
+      <h3>{props.fossil.genus}</h3>
+      <div className="fossilImage">
+        {props.fossil.image == null || props.fossil.image == "" ?
+          null : <img className="fossil--image" src={props.fossil.image} alt="Fossil Hunt Logo" />}
 
 
+      </div>
+      <div className="locationInfo">Location: {props.fossil.location}</div>
 
-
-<Link to={`/fossilEdit/${props.fossil.id}`}>
-  <button className="detailsEdit">Edit</button>
-</Link>
+      <section className="btn">
+        <Link to={`/fossilDetails/${props.fossil.id}`}>
+          <button className="detailsButton">Details</button>
+        </Link>
 
 
 
-        <button className="deleteButton" onClick={()=> {props.deleteFossil(props.fossil.id)}}>Delete</button>
-        </section>
-        </section>
-    )
+
+        <Link to={`/fossilEdit/${props.fossil.id}`}>
+          <button className="detailsEdit">Edit</button>
+        </Link>
+
+
+
+        <button className="deleteButton" onClick={() => { props.deleteFossil(props.fossil.id) }}>Delete</button>
+      </section>
+    </section>
+  )
 }
 
 
